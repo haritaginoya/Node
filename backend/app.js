03e74d0182825d06b57ebc48f1a70ef72e87e73c
Kaklotar08 <vishalkaklotar08@gmail.com>
2025-05-30 12:41:19 +0530

    crud

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,6 @@ const express = require('express')
 require('dotenv').config(); 
 const port = process.env.PORT || 5000
 const connectDB = require('./config/db')
-console.log(port);
-console.log(process.env.PORT);
 
 connectDB()
 
@@ -14,14 +12,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-
 app.use('/api/goals',require('./routes/goalsRoutes'))
 app.use(errorHandler)
 
 app.listen(port,() =>{
     console.log(`Server Started http://localhost:${port}`)
 })
-
 
 
 
